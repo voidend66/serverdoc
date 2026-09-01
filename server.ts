6333311d81d54eb9ac926e18600f352e261d3d23
voidend66 <msmoal64@gmail.com>
2026-09-01 22:09:11 +0330

    .

--- server.ts
+++ server.ts
@@ -7,7 +7,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 8045;
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
